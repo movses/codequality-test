c435c31bbec0d3832832aa98f760dab1a4ec669f
movses <movses.saponjyan@gmail.com>
2025-10-20 16:49:14 +0400

    test: parallelsim

---
+++ repetition/7.py
@@ -0,0 +1,27 @@
+def process_data():
+    data = [1, 2, 3]
+    total = sum(data)
+    print("Processed data:", total)
+
+def process_data():
+    data = [1, 2, 3]
+    total = sum(data)
+    print("Processed data:", total)
+
+def process_data():
+    data = [1, 2, 3]
+    total = sum(data)
+    print("Processed data:", total)
+
+def process_data():
+    data = [1, 2, 3]
+    total = sum(data)
+    print("Processed data:", total)
+
+def process_data():
+    data = [1, 2, 3]
+    total = sum(data)
+    print("Processed data:", total)
+
+process_data()
+
