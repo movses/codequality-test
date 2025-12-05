3e2ecef16584c3c571de75b8684ebfe62e900af1
movses <movses.saponjyan@gmail.com>
2025-12-05 12:47:32 +0400

    b

--- a/listCiScans.php
+++ a/listCiScans.php
@@ -1,15 +1,20 @@
 <?php
+
 namespace controllers\public_api\rest\reports\v1;
+
 use Core\input_validation\ObjectFieldValidation;
 use League\Route\Http\Exception\BadRequestException;
+
 class listCiScans implements \controllers\controllerInterface
 {
 	public function __construct(
 		private \App\repositories\integrations\continuous_integration\scanRuns $ciScanRunsRepo,
 	){}
+
 	public function handleRequest($request, $args, $response)
 	{
 		$getvars = $request->getQueryParams();
+
 		$page = $getvars['page'] ?? 0;
 		$page = ObjectFieldValidation::assertFieldOnObjectIsPositiveInteger(['page' => $page], 'page');
 		
@@ -23,9 +28,12 @@ class listCiScans implements \controllers\controllerInterface
 		if($filter_code_repo_id) {
 			$filter_code_repo_id = ObjectFieldValidation::assertFieldOnObjectIsPositiveInteger(['filter_code_repo_id' => $filter_code_repo_id], 'filter_code_repo_id');
 		}
+
 		$filter_gate_status = $getvars['filter_gate_status'] ?? 'all';
 		$filter_gate_status = ObjectFieldValidation::assertFieldOnObjectIsInArray(['filter_gate_status' => $filter_gate_status], 'filter_gate_status', ['all', 'timed_out', 'unknown', 'passed', 'failed', 'bypassed', 'pending']);
+
 		$search = trim($getvars['search'] ?? '');
+
 		$ci_scan_runs = $this->ciScanRunsRepo->listCiScanRuns(
 			page: $page,
 			per_page: $per_page,
@@ -33,19 +41,23 @@ class listCiScans implements \controllers\controllerInterface
 			repo_id: $filter_code_repo_id,
 			gate_status: $filter_gate_status,
 		);
+
 		$pull_requests_ci_checks = $this->formatCiScanRuns($ci_scan_runs);
+
 		return $pull_requests_ci_checks;
+
 	}
+
 	private function formatCiScanRuns($ci_scan_runs)
 	{
 		$formatted_ci_scan_runs = [];
 
 		foreach ($ci_scan_runs as $ci_scan_run) {
-			$filter_gate_status = $this->getGateStatus($ci_scan_run);
+			$gate_status = $this->getGateStatus($ci_scan_run);
 
 			$formatted_ci_scan_run = [
 				'scan_id' => $ci_scan_run['scan_id'],
-				'gate_status' => $filter_gate_status,
+				'gate_status' => $gate_status,
 				'new_issues_count' => $ci_scan_run['new_issues_count'],
 				'solved_issues_count' => $ci_scan_run['solved_issues_count'],
 				'started_at' => $ci_scan_run['started_at'],
@@ -53,21 +65,27 @@ class listCiScans implements \controllers\controllerInterface
 				'code_repo_id' => $ci_scan_run['scm_repo_id'],
 				'code_repo_name' => $ci_scan_run['scm_repo_name'],
 			];
+
 			$metadata = json_decode($ci_scan_run['metadata'], true);
+
 			$formatted_ci_scan_run['pull_request_title'] = $metadata['pull_request_metadata']['title'] ?? null;
 			$formatted_ci_scan_run['pull_request_url'] = $metadata['pull_request_metadata']['html_url'] ?? $metadata['pull_request_metadata']['url'] ?? null;
 			$formatted_ci_scan_run['branch_name'] = $metadata['ref'] ?? null;
 			$formatted_ci_scan_run['is_release_gating'] = $metadata['is_release_gating'] ?? false;
+
+
 			$formatted_ci_scan_runs[] = $formatted_ci_scan_run;
 		}
+
 		return $formatted_ci_scan_runs;
 	}
+
 	private function getGateStatus($ci_scan_run)
 	{
 		if ($ci_scan_run['status'] == 'timed_out') {
 			return 'timed_out';
 		}
 
-		return $ci_scan_run['filter_gate_status'];
+		return $ci_scan_run['gate_status'];
 	}
 }
