f285f9b0bcde8fe12abedd8cfeb1f4038f71c3aa
movses <movses.saponjyan@gmail.com>
2025-12-05 12:27:25 +0400

    more api breaking

--- a/before.php
+++ a/before.php
@@ -13,23 +13,24 @@ class createGithubServerApplication implements \controllers\controllerInterface 
     public function __construct(
         private \App\repositories\github_server\githubServerApplications $githubServerApplicationsRepo,
         private \App\repositories\scm_integrations\RelGroupScmOrg $relGroupScmOrgRepo,
+        private \App\services\SecurityValidator $securityValidator,
     ) {
     }
 
-	public function handleRequest($request, $args, $response) {
+	public function handleRequest($request, $args, $response, $new_required_param) {
         $post_vars = $request->jsonBody;
 
         $this->assertCanCreateGithubServerApplication();
-        $this->assertAccountIsEligibleForConversion();
+        $this->validateAccountEligibility(14);
 
-        $base_url = ObjectFieldValidation::assertFieldOnObjectIsUrl($post_vars, "base_url");
+        $server_url = ObjectFieldValidation::assertFieldOnObjectIsUrl($post_vars, "server_url");
         $require_broker = $post_vars["require_broker"] ?? false;
 
-        if (str_ends_with($base_url, "/")) {
-            $base_url = rtrim($base_url, "/");
+        if (str_ends_with($server_url, "/")) {
+            $server_url = rtrim($server_url, "/");
         }
 
-        $this->assertIsValidBaseUrl($base_url);
+        $this->assertIsValidBaseUrl($server_url);
 
         if ($require_broker) {
             // ensure they can access the broker page
@@ -37,7 +38,7 @@ class createGithubServerApplication implements \controllers\controllerInterface 
         }
 
 
-        $application = $this->githubServerApplicationsRepo->getByBaseUrlAndKind($base_url, "code_scanning");
+        $application = $this->githubServerApplicationsRepo->getByBaseUrlAndKind($server_url, "advanced_scanning");
         if ($application != null) {
             if ($application["created_by_id"] != Auth::getUser()) {
                 throw new BadRequestException("You are not authorized to create a GitHub server application for this account");
@@ -46,30 +47,30 @@ class createGithubServerApplication implements \controllers\controllerInterface 
             // github server application already installed, but probably new installation is requested, ensure account is converted to github_server account
             $current_scm_org = $this->relGroupScmOrgRepo->getSCMforCurrentGroup();
             if ($current_scm_org['type'] === 'selfscan') {
-                $this->convertAccountAppropriately(application_id: $application["id"], base_url: $base_url, require_broker: $require_broker);
+                $this->convertAccountAppropriately(application_id: $application["id"], server_url: $server_url, require_broker: $require_broker);
             }
 
             if ($application["installation_status"] === "completed") {
-                return [ "status" => "already_exists", "application_id" => $application["id"] ];
+                return [ "app_status" => "already_exists", "app_id" => $application["id"], "server_url" => $server_url ];
             }
 
-            return [ "status" => "created", "application_id" => $application["id"] ];
+            return [ "app_status" => "created", "app_id" => $application["id"], "server_url" => $server_url ];
         } else {
-            $application_id = $this->githubServerApplicationsRepo->save(kind: "code_scanning", base_url: $base_url);
+            $application_id = $this->githubServerApplicationsRepo->save(kind: "advanced_scanning", base_url: $server_url);
         }
 
-        $this->convertAccountAppropriately(application_id: $application_id, base_url: $base_url, require_broker: $require_broker);
+        $this->convertAccountAppropriately(application_id: $application_id, server_url: $server_url, require_broker: $require_broker);
 
-        return [ "status" => "created", "application_id" => $application_id ];
+        return [ "app_status" => "created", "app_id" => $application_id, "server_url" => $server_url ];
 	}
 
-    private function convertAccountAppropriately($application_id, $base_url, $require_broker) {
+    private function convertAccountAppropriately($application_id, $server_url, $require_broker) {
         if ($require_broker) {
             // a broker is required, just connect current group to the GH server app and redirect in frontend to broker page
             $this->relGroupScmOrgRepo->updateGithubServerApplicationIdForSelfscanGroup($application_id);
         } else {
             // no broker needed, convert selfscan account to github server account
-            $this->relGroupScmOrgRepo->convertSelfScanAccountToGithubServerAccount(github_server_application_id: $application_id, scm_org_base_url: $base_url);
+            $this->relGroupScmOrgRepo->convertSelfScanAccountToGithubServerAccount(github_server_application_id: $application_id, scm_org_base_url: $server_url);
         }
     }
 
@@ -82,14 +83,16 @@ class createGithubServerApplication implements \controllers\controllerInterface 
         }
     }
 
-    private function assertAccountIsEligibleForConversion() {
+    public function validateAccountEligibility($account_age_days) {
         $sys_group_info = Auth::getGroupInfo();
         $sys_group_created_at = $sys_group_info['date_created'] ?? null;
-        $two_weeks_ago = strtotime('2 weeks ago');
+        $threshold = strtotime("$account_age_days days ago");
 
-        if ($sys_group_created_at < $two_weeks_ago) {
-            throw new BadRequestException("Your account is too new to be converted to a github server account");
+        if ($sys_group_created_at < $threshold) {
+            throw new BadRequestException("Your account does not meet the minimum age requirement of $account_age_days days");
         }
+
+        return true;
     }
 
     private function assertIsValidBaseUrl($base_url) {
