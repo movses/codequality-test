24561d37ef4ebe7cd858d00b70768f698ac10a6f
movses <movses.saponjyan@gmail.com>
2025-12-10 12:53:40 +0400

    call say goodbie

--- 1.py
+++ 1.py
@@ -28,7 +28,8 @@ def say_good_user(name):
 def main():
     greet_user("Alice")
     greet_user("Bob")
-
+    say_good_user("Alice")
+    say_good_user("Bob")
 
 if __name__ == "__main__":
     main()
