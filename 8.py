6595e6fa17829bd9c590fb314b9bc2acce8ccb44
Movses Saponjyan <movses.saponjyan@gmail.com>
2026-01-13 18:03:20 +0400

    Update 8.py

--- 8.py
+++ 8.py
@@ -10,14 +10,3 @@ def greet_user(name):
 
 # --- accidentally repeated code ends here ---
 
-def greet_user(name):
-    print(f"Hello, {name}!")
-
-# --- accidentally repeated code starts here ---
-
-def greet_user(name):
-    print(f"Hello, {name}!")
-
-
-
-
