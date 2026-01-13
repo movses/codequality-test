1eb67d805a7de215fbded83451981c4be9127d1d
Movses Saponjyan <movses.saponjyan@gmail.com>
2026-01-13 17:47:13 +0400

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,18 +1,4 @@
 
-def greet_user(name):
-    print(f"Hello, {name}!")
-
-
-def greet_user(name):
-    print(f"Hello, {name}!")
-
-def say_good_user(name):
-    print(f"Goodbye, {name}!")
-    print("Thanks for using our program.")
-
-def say_good_user(name):
-    print(f"Goodbye, {name}!")
-    print("Thanks for using our program.")
 
 
 def main():
